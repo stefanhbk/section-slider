d5136c18b66853acd128a993ca524e1940d9dd7e
StefanHbk <stefanvukovic89@gmail.com>
2021-05-29 16:50:59 +0200

    Add logic for radio btn form

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,2 @@
-
+$(document).ready((function(){$(".form__label-radio").on("click",(function(){let a=$(this),o=$(".form__radio-group");o.removeClass("active"),a.closest(o).hasClass("active")||a.closest(o).addClass("active")}))}));
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+	
+	// Radio btn form
+	$('.form__label-radio').on('click', function() {
+		
+		let $this = $(this),
+			$formLabel = $('.form__radio-group');
 
+		$formLabel.removeClass('active');
+
+		if (!$this.closest($formLabel).hasClass('active')) {
+			$this.closest($formLabel).addClass('active');
+		}
+	});
+});
